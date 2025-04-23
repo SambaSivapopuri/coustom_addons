3d22d0f8312d3e6d89b1226e41476840d30996d1
alexkatta123 <alexkatta123@gmail.com>
2025-04-23 15:47:29 +0530

    update

--- game_zone/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .import event_management
-from .import customer_details
-from .import ticket_management
--- sports_zone/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .import event_management
-from .import customer_management
-from .import ticket_management
