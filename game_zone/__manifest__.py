988e6de5669f27c98dfeb1e563118aa5a72c27ed
alexkatta123 <alexkatta123@gmail.com>
2025-04-23 15:57:54 +0530

    update

--- game_zone/__manifest__.py
+++ game_zone/__manifest__.py
@@ -1,15 +1,4 @@
 {
     'name': 'Game Zone', 
-    'summary': 'Game Zone',
-    'description': '''
-        test
-    ''',
-    'version': '1',
-    'license': 'LGPL-3', 
-    'author': 'Game Zone',
     
-    
-    'data': [
-        
-    ]
 }
