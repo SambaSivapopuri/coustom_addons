007a19fb5a04dceb7be6458663389d6bbece54f4
alexkatta123 <alexkatta123@gmail.com>
2025-04-23 15:51:59 +0530

    update

---
+++ game_zone/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Game Zone', 
+    'summary': 'Game Zone',
+    'description': '''
+        
+    ''',
+    'version': '1',
+    'category': 'Themes/Backend', 
+    'license': 'LGPL-3', 
+    'author': 'MuK IT',
+    
+    'depends': [
+        
+    ],
+    'data': [
+        
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
